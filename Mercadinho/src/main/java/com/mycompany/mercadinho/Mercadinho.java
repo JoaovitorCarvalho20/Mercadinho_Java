fe56f26eedb1c3f4d0ec8075ab738fa4cde56222
joaov <joaov@10.6.7.47>
2023-10-04 00:11:13 -0300

    final dos testes

--- Mercadinho/src/main/java/com/mycompany/mercadinho/Mercadinho.java
+++ Mercadinho/src/main/java/com/mycompany/mercadinho/Mercadinho.java
@@ -4,12 +4,6 @@ package com.mycompany.mercadinho;
 public class Mercadinho {
 
     public static void main(String[] args) {
-        System.out.println("teste do repositorio");
-        System.out.println("teste do repositorio");
-        System.out.println("serio ");
-        System.out.println("teste");       
-        System.out.println("teste");
-
-
+       
     }
 }
