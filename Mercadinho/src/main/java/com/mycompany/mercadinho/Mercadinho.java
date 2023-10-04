4069cca7e3bf2d6630fac523dcfd4663085331e2
joaov <joaov@10.6.7.47>
2023-10-03 23:49:13 -0300

    Teste do repositorio 3

--- Mercadinho/src/main/java/com/mycompany/mercadinho/Mercadinho.java
+++ Mercadinho/src/main/java/com/mycompany/mercadinho/Mercadinho.java
@@ -6,6 +6,7 @@ public class Mercadinho {
     public static void main(String[] args) {
         System.out.println("teste do repositorio");
         System.out.println("teste do repositorio");
+        System.out.println("serio ");
 
     }
 }
