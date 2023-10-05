f0e058852d00a182c071364887ed863d3f66f0a0
joaov <joaov@10.6.7.47>
2023-10-05 16:32:08 -0300

    Criação da classe Login

---
+++ Mercadinho/src/main/java/com/mycompany/mercadinho/Login.java
@@ -0,0 +1,79 @@
+package com.mycompany.mercadinho;
+
+import java.util.Scanner;
+
+// Declaração da classe Login
+public class Login {
+    // Declaração de variáveis de instância privadas
+    private String nome;
+    private String email;
+    private String seenha;
+
+    // Declaração de um objeto Scanner para entrada de dados
+    Scanner input = new Scanner(System.in);
+
+    // Construtor da classe Login (não inicializa corretamente as variáveis)
+    public Login() {
+        this.nome = nome;
+        this.email = email;
+        this.seenha = seenha;
+    }
+
+    // Métodos getters e setters para as variáveis nome, email e seenha
+
+    // Método para configurar o nome
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Método para obter o nome
+    public String getNome() {
+        return nome;
+    }
+
+    // Método para configurar o email
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Método para obter o email
+    public String getEmail() {
+        return email;
+    }
+
+    // Método para configurar a senha (você escreveu "seenha" em vez de "senha")
+    public void setSeenha(String senha) {
+        this.seenha = senha;
+    }
+
+    // Método para obter a senha (você escreveu "seenha" em vez de "senha")
+    public String getSeenha() {
+        return seenha;
+    }
+
+    // Método para realizar o cadastro do usuário
+    public void cadastro() {
+        System.out.println("Entre com o nome do usuário");
+        String nome = input.nextLine();
+        setNome(nome);
+
+        System.out.println("Entre com o email");
+        String email = input.next();
+        setEmail(email);
+
+        System.out.println("Entre com a senha");
+        String senha = input.next();
+        setSeenha(senha);
+    }
+
+    // Método para verificar o cadastro e conceder acesso com base nos valores fornecidos
+    public void verificarCadastro() {
+        if (getNome().equals("ADM") && getEmail().equals("adm@teste") && getSeenha().equals("senha123")) {
+            System.out.println("Acesso aprovado ao administrador");
+        } else if (getNome().equals("Fun") && getEmail().equals("fun@teste") && getSeenha().equals("senha456")) {
+            System.out.println("Acesso aprovado ao funcionário");
+        } else {
+            System.out.println("Acesso negado. Tente novamente.");
+        }
+    }
+}
--- Mercadinho/src/main/java/com/mycompany/mercadinho/Mercadinho.java
+++ Mercadinho/src/main/java/com/mycompany/mercadinho/Mercadinho.java
@@ -4,6 +4,9 @@ package com.mycompany.mercadinho;
 public class Mercadinho {
 
     public static void main(String[] args) {
-        System.out.println("teste");
+        Login logar=new Login();
+        logar.cadastro();
+        logar.verificarCadastro();
+
     }
 }
