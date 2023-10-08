1b9c7729d64a23c3569d4fce7c8d9a83cf4b3d98
João Vitor Carvalho <100238388+JoaovitorCarvalho20@users.noreply.github.com>
2023-10-08 11:46:05 -0300

    Criação da Classe produto

---
+++ Mercadinho/src/main/java/com/mycompany/mercadinho/Estoque.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mercadinho;
+
+/**
+ *
+ * @author joaov
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Estoque {
+    private Map<Produto, Integer> produtosQuantidade;
+
+    public Estoque() {
+        produtosQuantidade = new HashMap<>();
+    }
+
+    public void adicionarProduto(Produto produto, int quantidade) {
+        if (quantidade <= 0) {
+            System.out.println("A quantidade deve ser maior que zero.");
+            return;
+        }
+
+        if (produtosQuantidade.containsKey(produto)) {
+            int quantidadeExistente = produtosQuantidade.get(produto);
+            produtosQuantidade.put(produto, quantidadeExistente + quantidade);
+            System.out.println(quantidade + " unidades de " + produto.getNomeProduto() + " adicionadas ao estoque.");
+        } else {
+            produtosQuantidade.put(produto, quantidade);
+            System.out.println(quantidade + " unidades de " + produto.getNomeProduto() + " adicionadas ao estoque.");
+        }
+    }
+
+    public void removerProduto(Produto produto, int quantidade) {
+        if (quantidade <= 0) {
+            System.out.println("A quantidade deve ser maior que zero.");
+            return;
+        }
+
+        if (produtosQuantidade.containsKey(produto)) {
+            int quantidadeExistente = produtosQuantidade.get(produto);
+            if (quantidadeExistente >= quantidade) {
+                produtosQuantidade.put(produto, quantidadeExistente - quantidade);
+                System.out.println(quantidade + " unidades de " + produto.getNomeProduto() + " removidas do estoque.");
+            } else {
+                System.out.println("Quantidade insuficiente em estoque para remover.");
+            }
+        } else {
+            System.out.println("Produto não encontrado em estoque.");
+        }
+    }
+
+    public void atualizarProduto(Produto produto, int novaQuantidade) {
+        if (novaQuantidade <= 0) {
+            System.out.println("A nova quantidade deve ser maior que zero.");
+            return;
+        }
+
+        produtosQuantidade.put(produto, novaQuantidade);
+        System.out.println("Quantidade de " + produto.getNomeProduto() + " atualizada para " + novaQuantidade + " unidades.");
+    }
+
+    public int verificarEstoque(Produto produto) {
+        int quantidade = produtosQuantidade.getOrDefault(produto, 0);
+        System.out.println("Quantidade de " + produto.getNomeProduto() + " em estoque: " + quantidade + " unidades.");
+        return quantidade;
+    }
+
+    public void exibirEstoque() {
+        System.out.println("=== Estoque Atual ===");
+        for (Map.Entry<Produto, Integer> entry : produtosQuantidade.entrySet()) {
+            Produto produto = entry.getKey();
+            int quantidade = entry.getValue();
+            System.out.println(produto.toString() + " - Quantidade em estoque: " + quantidade + " unidades.");
+        }
+    }
+}
--- Mercadinho/src/main/java/com/mycompany/mercadinho/Mercadinho.java
+++ Mercadinho/src/main/java/com/mycompany/mercadinho/Mercadinho.java
@@ -10,6 +10,10 @@ public class Mercadinho {
         Produto p1=new Produto("ARROZ", "A1", 14.0,1);
         Produto p2=new Produto("FEIJAO", "A", 13.9, 2);
         System.out.println(p1.toString());
+        Estoque estoque=new Estoque();
+        estoque.adicionarProduto(p2, 45);
+        estoque.adicionarProduto(p1, 34);
+        estoque.exibirEstoque();
 
     }
 }
